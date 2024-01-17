11d7332827aa2f0ce1f728a795cc2fbda73d714b
THANH NGUYEN <thanh.nguyenchisong@emeal.nttdata.com>
2024-01-17 15:38:25 +0700

    - Commit

--- comic-service/src/main/java/com/sky/tv/comics/anotation/AnalysisAspect.java
+++ comic-service/src/main/java/com/sky/tv/comics/anotation/AnalysisAspect.java
@@ -1,23 +1,6 @@
 package com.sky.tv.comics.anotation;
 
-import com.sky.tv.comics.anotation.mesurement.LogExecutionTime;
-import com.sky.tv.comics.entity.Comic;
-import com.sky.tv.comics.entity.ComicAnalysis;
-import com.sky.tv.comics.entity.PeriodTypeEnum;
-import com.sky.tv.comics.exception.ResourceNotFoundException;
-import com.sky.tv.comics.repository.ComicAnalysisRepo;
-import com.sky.tv.comics.repository.ComicRepo;
 import com.sky.tv.comics.service.ComicAnalysisService;
-import com.sky.tv.comics.utils.DateServiceUtils;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.temporal.TemporalAdjusters;
-import java.time.temporal.WeekFields;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -36,20 +19,8 @@ public class AnalysisAspect {
 
     @Around("@annotation(analysis)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint, Analysis analysis) throws Throwable {
-        LocalDate today = LocalDate.now();
-        LocalDate monday = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
-        LocalDate mondayNext = today.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
-        Date startTime = Date.from(monday.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Date endTime = Date.from(mondayNext.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        List<ComicAnalysis> comicAnalyses = comicAnalysisService.getCAByStartDate(startTime);
-        if(comicAnalyses.size() > 1) {
-            log.warn("There are more than 1 ComicAnalysis record for this week");
-        }
-        ComicAnalysis comicAnalysis = comicAnalyses.stream().findFirst().orElse(null);
-        if (comicAnalysis == null) {
-            UUID comicID = (UUID) joinPoint.getArgs()[0];
-            comicAnalysisService.create(comicID, startTime, endTime);
-        }
+        UUID comicID = (UUID) joinPoint.getArgs()[0];
+        comicAnalysisService.preJobView(comicID);
         return joinPoint.proceed();
     }
 
--- comic-service/src/main/java/com/sky/tv/comics/service/ComicAnalysisServiceImpl.java
+++ comic-service/src/main/java/com/sky/tv/comics/service/ComicAnalysisServiceImpl.java
@@ -7,15 +7,22 @@ import com.sky.tv.comics.entity.PeriodTypeEnum;
 import com.sky.tv.comics.exception.ResourceNotFoundException;
 import com.sky.tv.comics.repository.ComicAnalysisRepo;
 import com.sky.tv.comics.repository.ComicRepo;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.query.spi.Limit;
 import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ComicAnalysisServiceImpl implements ComicAnalysisService {
 
   @Autowired
@@ -25,25 +32,42 @@ public class ComicAnalysisServiceImpl implements ComicAnalysisService {
   private ComicRepo comicRepo;
 
   @Override
-  public void create(UUID comicID, Date startTime, Date endTime) {
+  public ComicAnalysis create(UUID comicID, Date startTime, Date endTime) {
     Comic comic =  comicRepo.findById(comicID).orElseThrow(() -> new ResourceNotFoundException("Comic", "id", comicID.toString()));
     ComicAnalysis comicAnalysis = new ComicAnalysis();
     comicAnalysis.setComic(comic);
     comicAnalysis.setPeriodType(PeriodTypeEnum.WEEK);
     comicAnalysis.setStartTime(startTime);
     comicAnalysis.setEndTime(endTime);
-    comicAnalysisRepo.save(comicAnalysis);
+    return comicAnalysisRepo.save(comicAnalysis);
   }
 
   @Override
   public void updateViewNumber(ComicAnalysis comicAnalysis, int number) {
-    Comic comic =  comicRepo.findById(comicID).orElseThrow(() -> new ResourceNotFoundException("Comic", "id", comicID.toString()));
-    ComicAnalysis comicAnalysis = new ComicAnalysis();
-    comicAnalysis.setComic(comic);
-    comicAnalysis.setPeriodType(PeriodTypeEnum.WEEK);
-    comicAnalysis.setStartTime(startTime);
-    comicAnalysis.setEndTime(endTime);
+    int comicViewNumber = comicAnalysis.getComic().getNumberView();
+    int analysisViewNumber = comicAnalysis.getNumberView();
+    comicAnalysis.getComic().setNumberView(comicViewNumber + number);
+    comicAnalysis.setNumberView(analysisViewNumber + number);
     comicAnalysisRepo.save(comicAnalysis);
+  }
+
+  @Override
+  @Async
+  public void preJobView(UUID comicID) {
+    LocalDate today = LocalDate.now();
+    LocalDate monday = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+    LocalDate mondayNext = today.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+    Date startTime = Date.from(monday.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    Date endTime = Date.from(mondayNext.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    List<ComicAnalysis> comicAnalyses = getCAByStartDate(startTime);
+    if(comicAnalyses.size() > 1) {
+      log.warn("There are more than 1 ComicAnalysis record for this week");
+    }
+    ComicAnalysis comicAnalysis = comicAnalyses.stream().findFirst().orElse(null);
+    if (comicAnalysis == null) {
+      comicAnalysis = create(comicID, startTime, endTime);
+    }
+    updateViewNumber(comicAnalysis, 1);
   }
 
   @Override
