048e28497ae6214620f30bc6d4540b9451f18a78
THANH NGUYEN <thanh.nguyenchisong@emeal.nttdata.com>
2024-02-01 18:21:27 +0700

    - Commit

--- comic-service/src/main/java/com/sky/tv/comics/controller/CategoryController.java
+++ comic-service/src/main/java/com/sky/tv/comics/controller/CategoryController.java
@@ -1,25 +1,19 @@
 package com.sky.tv.comics.controller;
 
-import com.sky.tv.comics.dto.response.ResponseDefault;
 import com.sky.tv.comics.dto.CategoryDTO;
+import com.sky.tv.comics.dto.response.ResponseDefault;
 import com.sky.tv.comics.exception.BusinessException;
 import com.sky.tv.comics.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import java.util.List;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
